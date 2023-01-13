ebb0fd528ff8a6a6d351fedbe865bdd8ba07103d
Artiom Nazarchuk <sel_it@tut.by>
2023-01-13 14:29:37 +0300

    Connecting to the API

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = '949343008822928ceceaa147a4fd4750'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = 'Minsk'
+    res = requests.get(url.format(city)).json()
+    print(res)
     return render(request, 'weather/index.html')
 
 def about(request):
