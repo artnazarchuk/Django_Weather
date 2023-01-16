759363fabcb8065c2ced9d41a11862e3d88662b4
Artiom Nazarchuk <sel_it@tut.by>
2023-01-16 11:30:42 +0300

    Adding a database of cities and displaying weather information

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -4,10 +4,18 @@ import requests
 def index(request):
     appid = '949343008822928ceceaa147a4fd4750'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = 'Minsk'
+    city = 'Mozyr'
     res = requests.get(url.format(city)).json()
-    print(res)
-    return render(request, 'weather/index.html')
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
 
 def about(request):
     return render(request, 'weather/about.html')
