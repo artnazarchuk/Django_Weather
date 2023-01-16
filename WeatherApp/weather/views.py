96a5206c1e9dd0aa70c6efad89c78524f8300905
Artiom Nazarchuk <sel_it@tut.by>
2023-01-16 16:23:55 +0300

    Connection with the database of cities and data output

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = '949343008822928ceceaa147a4fd4750'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = 'Mozyr'
-    res = requests.get(url.format(city)).json()
 
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon'],
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon'],
+        }
+
+        all_cities.append(city_info)
+
+    context = {
+        'all_info': all_cities,
+        'form': form,
     }
-
-    context = {'info': city_info}
 
     return render(request, 'weather/index.html', context)
 
